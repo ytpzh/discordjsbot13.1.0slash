1cfded96abf3277b3cff2f0d80a1aa4303b4adc9
PZH <contact@pzh.fr>
2021-08-24 23:16:43 +0200

    Create choice.js

---
+++ commands/demo/choice.js
@@ -0,0 +1,18 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('choice')
+        .setDescription('Choice command')
+        .addStringOption(option =>
+        option.setName('choice_reply')
+            .setDescription('Choose one of these options')
+            .setRequired(true)
+            .addChoice('Apple', 'This is an apple :apple')
+            .addChoice('Banana', 'This is a banana :banana:')
+            .addChoice('Peach', 'This is a peach :peach:')
+            .addChoice('PZH', `PZH ??? That's not a fruit, but my creator ! :star_struck:`)),
+    async execute(interaction) {
+        await interaction.reply(interaction.options.getString('choice'));
+    },
+};
