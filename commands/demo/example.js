65ff1adf6d1d9ddd703eaea4bafd7d1ee2715d59
PZH <contact@pzh.fr>
2021-08-24 22:28:44 +0200

    Update and rename simple_reply.js to example.js

---
+++ commands/demo/example.js
@@ -0,0 +1,10 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('example')
+        .setDescription('Example command'),
+    async execute(interaction) {
+        await interaction.reply('This is an example command ! :\D');
+    },
+};
