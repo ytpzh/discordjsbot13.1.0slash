cfccf212b41f6ea29963b480236db07bc771a365
PZH <contact@pzh.fr>
2021-08-24 22:32:28 +0200

    Update ephemeral.js

--- commands/demo/ephemeral.js
+++ commands/demo/ephemeral.js
@@ -1,1 +1,10 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
 
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('ephemeral')
+        .setDescription('Ephemeral command'),
+    async execute(interaction) {
+        await interaction.reply({content: 'Only you can see this message ! :\)', ephemeral: true});
+    },
+};
