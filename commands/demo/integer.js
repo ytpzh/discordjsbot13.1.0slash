9f218aa61c350756d2d922f68efea82473fe028b
PZH <contact@pzh.fr>
2021-08-24 23:17:05 +0200

    Create integer.js

---
+++ commands/demo/integer.js
@@ -0,0 +1,27 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('integer')
+        .setDescription('Integer command')
+        .addIntegerOption(option => option.setName('integer').setDescription('Enter an integer')),
+
+    async execute(interaction) {
+
+        let result = interaction.options.getInteger('integer')
+
+        if((result) < 0 || (result) > 5 ) return interaction.reply(`Choose a number between 0 and 5.`)
+
+        let roll = Math.floor(Math.random() * 6)
+
+        if(roll === result) {
+            await interaction.reply(`We choose the same number !! The number **${roll}**`);
+
+        } else {
+            await interaction.reply(`We don't choose the same number. :\( You choose **${result}** and me **${roll}**.`);
+
+        }
+
+    },
+
+};
