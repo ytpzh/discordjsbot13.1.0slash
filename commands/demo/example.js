c4e54f86113cb14f78bf13d5b64603d572249fb7
PZH <contact@pzh.fr>
2021-08-25 00:03:22 +0200

    Update example.js

--- commands/demo/example.js
+++ commands/demo/example.js
@@ -1,10 +1,15 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
 
 module.exports = {
+    
     data: new SlashCommandBuilder()
         .setName('example')
         .setDescription('Example command'),
+    
     async execute(interaction) {
+        
         await interaction.reply('This is an example command ! :\D');
+        
     },
+    
 };
