cf4cccd4dabaf591673caa532808b72a0b9c5653
PZH <contact@pzh.fr>
2021-08-25 15:17:27 +0200

    Create smug.js

---
+++ commands/images/smug.js
@@ -0,0 +1,33 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const superagent = require('superagent');
+
+module.exports = {
+
+    data: new SlashCommandBuilder()
+        .setName('smug')
+        .setDescription('Smug image'),
+
+    async execute(interaction) {
+
+        try {
+
+            const { body } = await superagent.get("https://nekos.life/api/v2/img/smug");
+
+            const smug_embed = {
+                color: 0x21C180,
+                description: `:slight_smile: **${interaction.user.username}** smugs`,
+                image: {url: body.url},
+                timestamp: new Date()
+            };
+
+            await interaction.reply({ embeds: [smug_embed] });
+
+        }catch(e){
+
+            console.error(e)
+
+        }
+
+    },
+
+};
