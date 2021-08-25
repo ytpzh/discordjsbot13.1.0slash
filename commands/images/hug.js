0247c6b1322830e4f6d22e655b87d0f4f7058f49
PZH <contact@pzh.fr>
2021-08-25 15:06:43 +0200

    Create hug.js

---
+++ commands/images/hug.js
@@ -0,0 +1,33 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const superagent = require('superagent');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('hug')
+        .setDescription('Hug a user')
+        .addUserOption(option => option.setName('user').setDescription('User to hug').setRequired(true)),
+
+    async execute(interaction) {
+
+        try {
+
+            const { body } = await superagent.get("https://nekos.life/api/v2/img/cuddle");
+
+            const hug_embed = {
+                color: 0x21C180,
+                description: `:kissing_smiling_eyes: **${interaction.user.username}** hugs **${interaction.options.getMember('user').user.username}**`,
+                image: {url: body.url},
+                timestamp: new Date()
+            };
+
+            await interaction.reply({ embeds: [hug_embed] });
+
+        }catch(e){
+
+            console.error(e)
+          
+        }
+      
+    },
+  
+};
