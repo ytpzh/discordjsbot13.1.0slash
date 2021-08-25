4869dc7b605cc2ca65840fd7e4f4e97498c4785e
PZH <contact@pzh.fr>
2021-08-25 15:16:48 +0200

    Create slap.js

---
+++ commands/images/slap.js
@@ -0,0 +1,34 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+const superagent = require('superagent');
+
+module.exports = {
+
+    data: new SlashCommandBuilder()
+        .setName('slap')
+        .setDescription('Slap a user')
+        .addUserOption(option => option.setName('user').setDescription('User to slap').setRequired(true)),
+
+    async execute(interaction) {
+
+        try {
+
+            const { body } = await superagent.get("https://nekos.life/api/v2/img/slap");
+
+            const slap_embed = {
+                color: 0x21C180,
+                description: `:hand_splayed: **${interaction.user.username}** slaps **${interaction.options.getMember('user').user.username}**`,
+                image: {url: body.url},
+                timestamp: new Date()
+            };
+
+            await interaction.reply({ embeds: [slap_embed] });
+
+        }catch(e){
+
+            console.error(e)
+
+        }
+
+    },
+
+};
