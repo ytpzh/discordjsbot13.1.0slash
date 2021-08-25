3abe92175cf22c75792e7f072405874cf43db5c5
PZH <contact@pzh.fr>
2021-08-25 15:54:39 +0200

    Create avatar.js

---
+++ commands/images/avatar.js
@@ -0,0 +1,26 @@
+const { SlashCommandBuilder } = require('@discordjs/builders');
+
+module.exports = {
+  
+    data: new SlashCommandBuilder()
+        .setName('avatar')
+        .setDescription('Show avatar of a user')
+        .addUserOption(option => option.setName('user').setDescription('Enter user')),
+
+    async execute(interaction) {
+
+        let user_i = interaction.options.getMember('user') || interaction;
+        let avatar = user_i.user.displayAvatarURL({dynamic : true});
+
+        const avatar_embed = {
+            color: 0x2c2f33,
+            description: `[Link](${avatar}?size=4096)`,
+            image: {url: `${avatar}?size=4096`},
+            timestamp: new Date()
+        };
+
+        await interaction.reply({ embeds: [avatar_embed] });
+
+    },
+
+};
